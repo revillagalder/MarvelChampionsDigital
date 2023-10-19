e2effd763b6c9fbf1e2488a29ec482e781112263
revillagalder <revillagalder@gmail.com>
2023-10-19 16:58:26 +0200

    Primer commit backend

---
+++ MarvelChampionsDigital-backend/src/main/java/com/mcd/MarvelChampionsDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelChampionsDigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelChampionsDigitalBackendApplication.class, args);
+	}
+
+}
---
+++ MarvelChampionsDigital-backend/src/test/java/com/mcd/MarvelChampionsDigitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelChampionsDigitalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
